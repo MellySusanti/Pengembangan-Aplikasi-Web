ad2103093b1e0572f46694b8b1f758328183ea9b
MellySusanti <mellyfang7713@gmail.com>
2021-11-22 21:35:06 +0700

    Pertemuan6 - REFS

--- src/App.js
+++ src/App.js
@@ -1,71 +1,30 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router,
-  Route,
-  Switch,
-  Link,
-  Redirect,
-  withRouter,
-  NavLink
-} from 'react-router-dom'
 
-//router dengan array
+// REFS
 
 class App extends Component{
   constructor(){
     super()
-    this.state = {
-      isAuh : false
-    }
+    this.myRef = React.createRef();
+  }
+  componentDidMount(){
+    this.myRef.current.style.height = '100vh';
+    this.myRef.current.style.width ='100%'
+  }
+  setBackground(bgColor, txtColor){
+    this.myRef.current.style.background = bgColor;
+    this.myRef.current.style.color = txtColor;
   }
   render(){
-    const LoginButton = withRouter(({history}) => (
-      <button onClick={() => {
-        this.setState({isAuth : true})
-        history.push('/profile')
-      }}>Login</button>
-    ))
-
-    const LogoutButton = withRouter(({history}) => (
-      <button onClick={() => {
-        this.setState({isAuth : false})
-        history.push('/login')
-      }}>Logout</button>
-    ))
-
-    const routes = [{
-      path : '/',
-      exact : true,
-      render : ()=> <div>Ini Halaman Home</div>
-    },{
-      path : '/news',
-      render : ()=> <div>Ini Halaman News</div>
-    },{
-      path : '/login',
-      render : ()=> <LoginButton />
-    },{
-      path : '/profile',
-      render : ()=> this.state.isAuth ? <div>Ini halaman Profile <br /><LogoutButton /></div> : <Redirect to ='/login' />
-    }]
-    return(
-      <Router>
-        <div>
-        <ul style={{listStyle: 'none'}}>
-             <li><NavLink exact activeStyle={{fontWeight:'bold', color:'red'}}to='/'>Home</NavLink></li>
-             <li><NavLink activeClassName='active-link' to='/news'>News</NavLink></li>
-             <li><NavLink activeClassName='active-link' to='/profile'>Profile</NavLink></li>
-           </ul>
-           <Switch>
-           {
-             routes.map((item, index) => (
-               <Route path={item.path} exact={item.exact} render={item.render} />
-             ))
-           }
-           </Switch>
-        </div>
-      </Router>
-    )
+    return (
+      <div ref={this.myRef}>
+        <p>Belajar pengembangan aplikasi web dengan Reactjs dengan topik Router dan Refs</p>
+        <button onClick={this.setBackground.bind(this, 'white', 'black')}>White</button>
+        <button onClick={this.setBackground.bind(this, 'red', 'white')}>Red</button>
+        <button onClick={this.setBackground.bind(this, 'green', 'white')}>Green</button>
+        <button onClick={this.setBackground.bind(this, 'blue', 'white')}>Blue</button>
+      </div>
+    );
   }
 }
 
