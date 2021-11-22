fe88251a02ab5f80f2fe00f7ee34422ba7bca4be
MellySusanti <mellyfang7713@gmail.com>
2021-11-22 20:41:34 +0700

    Pertemuan6 - Redirect

--- src/App.js
+++ src/App.js
@@ -4,28 +4,36 @@ import './App.css';
 import {BrowserRouter as Router,
   Route,
   Switch,
-  Link
+  Link,
+  Redirect
 } from 'react-router-dom'
 
-class App extends Component {
-  render(){
-    return(
-      <Router>
-        <div>
-          <ul style={{listStyle: 'none'}}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/news">News</Link></li>
-          </ul>
-
-          <Switch>
+class App extends Component{
+    constructor(){
+      super()
+      this.state ={
+        isAuth : false
+      }
+    }
+    render(){
+      return(
+        <Router>
+          <div>
+            <ul style={{listStyle: 'none'}}>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/news'>News</Link></li>
+              <li><Link to='/profile'>Profile</Link></li>
+            </ul>
+            <Switch>
             <Route path='/' exact render={() => <div> Ini halaman home</div>} />
-            <Route path='/news' render={() => <div>Ini halaman News</div>}/>          
-          </Switch>
-
-        </div>
-      </Router>
-    );
+            <Route path='/news' render={() => <div>Ini halaman News</div>}/>         
+            <Route path='/login' render={() => <div><button>Login</button></div>}/>
+            <Route path='/profile' render={() => this.state.isAuth ? <div>Ini halaman Profile</div> : <Redirect to='/login' />}/>
+            </Switch>
+          </div>
+        </Router>
+      )
+    }
   }
-}
-
+  
 export default App;
